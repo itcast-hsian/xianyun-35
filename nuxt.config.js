18de9c257cb2b02e164cc835171c74c41e75b6e8
itcast-hsian <541510140@qq.com>
2019-09-01 15:46:11 +0800

    第一次提交

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: '世界上最大的旅游网站',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'text/css', href: '//at.alicdn.com/t/font_1168872_ehvuah8v57g.css'}
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    'element-ui/lib/theme-chalk/index.css',
+    'assets/main.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '@/plugins/element-ui'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+    baseURL: "http://127.0.0.1:1337" // 新增axios默认请求路径
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    transpile: [/^element-ui/],
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/tab.vue
@@ -0,0 +1,71 @@
+<template>
+  <div style="padding:50px; margin-bottom:20px;">
+      <div class="tabs">
+          <span 
+          v-for="(item, index) in ['新闻', '体育', '娱乐']" 
+          :key="index"
+          @click="handleClick(index)"
+          :class="{ active: current === index }">
+              {{ item }}
+          </span>
+      </div>
+
+      <div v-show="current === 0">
+          <ul>
+              <li>中秋将至，</li>
+              <li>中秋将至，</li>
+              <li>中秋将至，</li>
+          </ul>
+      </div>
+
+      <div v-show="current === 1">
+          <ul>
+              <li>中国男篮世界杯</li>
+              <li>中国男篮世界杯</li>
+              <li>中国男篮世界杯</li>
+          </ul>
+      </div>
+
+      <div v-show="current === 2">
+          <ul>
+              <li>文章离婚</li>
+              <li>文章离婚</li>
+              <li>文章离婚</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+    data(){
+        return {
+            // 记录当前的状态
+            current: 0
+        }
+    },
+
+    methods: {
+        handleClick(index){
+            // 修改current
+            this.current = index;
+        }
+    }
+
+}
+</script>
+
+<style lang="less" scoped>
+    .tabs{
+        margin-bottom:20px;
+
+        span{
+            cursor: pointer;
+        }
+    }
+
+    .active{
+        color:red;
+    }
+</style>
