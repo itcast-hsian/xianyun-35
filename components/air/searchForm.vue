f83ab23d8e6394ae08203d4bac0a1b63a813a8f6
itcast-hsian <541510140@qq.com>
2019-09-05 15:07:15 +0800

    表单验证和跳转

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -212,7 +212,30 @@ export default {
 
         // 提交表单是触发
         handleSubmit(){
-           console.log(this.form)
+             const {departCity, destCity, departDate} = this.form;
+
+            // 判断输入框不能为空
+            if(!departCity){
+                this.$alert("出发城市不能为空", "提示");
+                return;
+            }
+
+            if(!destCity){
+                this.$alert("到达城市不能为空", "提示");
+                return;
+            }
+
+            if(!departDate){
+                this.$alert("出发日期不能为空", "提示");
+                return;
+            }
+
+            // 跳转到机票列表页 /air/flights
+            this.$router.push({
+                path: "/air/flights",
+                // url携带的参数
+                query: this.form
+            })
         }
     },
     mounted() {
