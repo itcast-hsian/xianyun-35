a68cbe1a2af018fa254ab19ae615dfa38d3ed3f0
itcast-hsian <541510140@qq.com>
2019-09-05 15:45:25 +0800

    特价机票

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -1,19 +1,19 @@
 <template>
   <section class="container">
-    <h2 class="air-title"><span class="iconfont iconfeiji"></span>
+    <h2 class="air-title">
+      <span class="iconfont iconfeiji"></span>
       <i>国内机票</i>
     </h2>
 
     <!-- 搜索广告栏 -->
     <el-row type="flex" justify="space-between">
-      
-        <!-- 搜索表单 -->
-        <SearchForm/>
+      <!-- 搜索表单 -->
+      <SearchForm />
 
-        <!-- banner广告 -->
-        <div class="sale-banner">
-            <img src="http://157.122.54.189:9093/images/pic_sale.jpeg">
-        </div>
+      <!-- banner广告 -->
+      <div class="sale-banner">
+        <img src="http://157.122.54.189:9093/images/pic_sale.jpeg" />
+      </div>
     </el-row>
 
     <!-- 广告 -->
@@ -39,134 +39,169 @@
 
     <!-- 特价机票 -->
     <div class="air-sale">
-        
-    </div>
 
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <!-- sales 后台返回的数据 -->
+        <el-col 
+        :span="6" 
+        v-for="(item, index) in sales" 
+        :key="index">
+          <nuxt-link
+            :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+          >
+            <!-- 封面图片 -->
+            <img :src="item.cover" />
+            <!-- 航班信息 -->
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{item.departCity}}-{{item.destCity}}</span>
+              <span>￥{{ item.price }}</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+
+    </div>
   </section>
 </template>
 
 <script>
-
 // 导入搜索栏组件
 import SearchForm from "@/components/air/searchForm";
 
 export default {
+  data(){
+    return {
+      sales: [] // 特价机票的数组
+    }
+  },
+
   components: {
-    SearchForm
+    SearchForm // 搜索组件
+  },
+
+  mounted(){
+    this.$axios({
+      url: "/airs/sale"
+    }).then(res => {
+      // 特价机票的列表
+      const {data} = res.data;
+
+      this.sales = data;
+    })
   }
-}
+};
 </script>
 
 <style scoped lang="less">
-.air-sale{
+.air-sale {
   border: 1px #ddd solid;
-  padding:20px;
-  margin-bottom:50px;
+  padding: 20px;
+  margin-bottom: 50px;
 
-  .air-sale-pic{
-    > div{
-      width:225px;
-      height:140px;
+  .air-sale-pic {
+    > div {
+      width: 225px;
+      height: 140px;
       position: relative;
       overflow: hidden;
 
-      img{
-        width:100%;
+      img {
+        width: 100%;
       }
 
-      .layer-bar{
-        position:absolute;
-        bottom:0;
-        left:0;
-        background: rgba(0,0,0,0.5);
-        color:#fff;
-        height:30px;
+      .layer-bar {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        background: rgba(0, 0, 0, 0.5);
+        color: #fff;
+        height: 30px;
         line-height: 30px;
-        width:100%;
+        width: 100%;
         box-sizing: border-box;
         padding: 0 15px;
         font-size: 14px;
 
-        span:last-child{
-          font-size:18px;
+        span:last-child {
+          font-size: 18px;
         }
       }
     }
   }
 }
 
-.air-sale-group{
-  margin-top:20px;
-  padding-top:8px;
-  border-right:1px #eee solid;
+.air-sale-group {
+  margin-top: 20px;
+  padding-top: 8px;
+  border-right: 1px #eee solid;
 
-  &:last-child{
-    border-right:none;
+  &:last-child {
+    border-right: none;
   }
 
-  .air-sale-row{
-    font-size:12px;
-    color:#666;
-    margin-bottom:8px;
+  .air-sale-row {
+    font-size: 12px;
+    color: #666;
+    margin-bottom: 8px;
 
-    .air-sale-price{
-      color:orange;
+    .air-sale-price {
+      color: orange;
       font-size: 20px;
     }
   }
 }
 
-.container{
-  width:1000px;
-  margin:0 auto;
+.container {
+  width: 1000px;
+  margin: 0 auto;
 }
 
-.air-title{
-  margin:15px 0;
-  font-size:20px;
+.air-title {
+  margin: 15px 0;
+  font-size: 20px;
   font-weight: normal;
-  color:orange;
+  color: orange;
 
-  span{
-    font-size:20px;
+  span {
+    font-size: 20px;
   }
 }
 
-.statement{
-  margin:15px 0;
-  border:1px #ddd solid;
-  background:#f5f5f5;
+.statement {
+  margin: 15px 0;
+  border: 1px #ddd solid;
+  background: #f5f5f5;
   height: 58px;
-  padding:10px 0;
-  box-sizing:border-box;
+  padding: 10px 0;
+  box-sizing: border-box;
 
-  > div{
+  > div {
     text-align: center;
     line-height: 38px;
-    border-right:1px #ddd solid;
+    border-right: 1px #ddd solid;
 
-    &:last-child{
+    &:last-child {
       border-right: none;
     }
 
-    *{
+    * {
       vertical-align: middle;
     }
 
-    i{
-      font-size:30px;
+    i {
+      font-size: 30px;
     }
   }
 }
 
-.air-sale-title{
-  margin:15px 0;
-  font-size:20px;
+.air-sale-title {
+  margin: 15px 0;
+  font-size: 20px;
   font-weight: normal;
-  color:#409EFF;
+  color: #409eff;
 
-  span{
-    font-size:20px;
+  span {
+    font-size: 20px;
   }
 }
 </style>
