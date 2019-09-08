234a96ada55f3b6abb7df7b30bae357dccbb1142
itcast-hsian <541510140@qq.com>
2019-09-08 15:56:46 +0800

    监听路由的变化

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -81,29 +81,40 @@ export default {
         FlightsAside
     },
 
+    watch: {
+        // watch可以监听this下的所有属性
+        $route(){
+            // 请求航班列表数据
+            this.getData();
+        }
+    },
+
     mounted(){
-
         // 请求航班列表数据
-        this.$axios({
-            url: "airs",
-            // 路由的url参数
-            params: this.$route.query
-        }).then(res => {
-            // 赋值给总数据
-            this.flightsData = res.data;
-
-            // 赋值给缓存总数据
-            this.cacheFlightsData = {...res.data};
-
-            // 分页的总条数
-            this.total =  this.flightsData.flights.length;
-
-            // 第一页的值
-            this.dataList = this.flightsData.flights.slice(0, this.pageSize);
-        })
+       this.getData();
     },
 
     methods: {
+        getData(){
+             // 请求航班列表数据
+            this.$axios({
+                url: "airs",
+                // 路由的url参数
+                params: this.$route.query
+            }).then(res => {
+                // 赋值给总数据
+                this.flightsData = res.data;
+
+                // 赋值给缓存总数据
+                this.cacheFlightsData = {...res.data};
+
+                // 分页的总条数
+                this.total =  this.flightsData.flights.length;
+
+                // 第一页的值
+                this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+            })
+        },
 
         // 该方法传递给子组件用于修改dataList
         setDataList(arr){
