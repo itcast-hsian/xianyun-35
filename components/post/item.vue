4547545daaf196ce5aafceee0479fd2600b9e240
itcast-hsian <541510140@qq.com>
2019-09-11 12:01:54 +0800

    组件递归

---
+++ components/post/item.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="digui-item">
+      <div v-for="(item, index) in data" :key="index">
+          {{item.title}}
+
+          <item :data="item.children" v-if="item.children"/>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // 可以属性是可选的属性，表示组件名, 是在组件内部可以根据该名字自己调用自己
+    name: "item",
+
+    props: ["data"]
+}
+</script>
+
+<style>
+.digui-item div{
+    padding-left:50px;
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,10 +1,56 @@
 <template>
-  <div>攻略首页</div>
+  <div style="padding:50px;">
+    <h2>组件递归</h2>
+
+    <Item :data="list"/>
+
+  </div>
 </template>
 
 <script>
+import Item from "@/components/post/item";
 export default {
+  data(){
+    return {
+      list: [
+        {
+          title: "衣服",
+          children: [
+            {
+              title: "男装",
+              children: [
+                {title: "秋裤"},
+                {title: "西装", children: [
+                  {title: "领带"},
+                  {title: "皮带"},
+                ]}
+              ]
+            },
+            {
+              title: "女装",
+              children: [
+                {title: "裙子"},
+                {title: "比基尼"}
+              ]
+            }
+          ]
+        },
 
+        {
+          title: "电器",
+          children: [
+            {title: "冰箱"},
+            {title: "空调"},
+            {title: "游戏机"}
+          ]
+        }
+      ]
+    }
+  },
+
+  components: {
+    Item
+  }
 }
 </script>
 
